d2097e0c063cb4f530e0ee5d5727ab3c74fad717
Corey <corey_924@yahoo.com.tw>
2015-06-24 18:59:07 +0800

    Added groups CRUD

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,45 @@
 class GroupsController < ApplicationController
   def index
+    @groups=Group.all
+  end
 
+  def show
+    @group=Group.find(params[:id])
+  end
+
+  def new
+    @group=Group.new
+  end
+
+  def edit
+    @group=Group.find(params[:id])
+  end
+
+  def create
+    @group=Group.new(group_params)
+    if @group.save
+      redirect_to groups_path, notice: 'Added successfully!'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @group=Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Modify successfully!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group=Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "Deleted successfully!"
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 end
