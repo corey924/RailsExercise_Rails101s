b564ee417d1b65f77c7dfe7f6a5ebae6568cb081
Corey <corey_924@yahoo.com.tw>
2015-07-05 19:22:20 +0800

    Added Create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @group=Group.find(params[:group_id])
+    @post=@group.posts.new(post_params)
+
+    if @post.save
+      redirect_to group_path(@group), notice: '新增文章成功！'
+    else
+      reder :new
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
